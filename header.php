e2fa3148e75ff055b6e688a8cd02d939c6617855
Cosmin Pavel | eMAG <cosmin.pavel@emag.ro>
2018-06-25 10:58:05 +0300

    first commit

---
+++ header.php
@@ -0,0 +1,28 @@
+
+<html>
+<head>
+    <title>Home</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="header.css">
+
+</head>
+
+<div id="border-img">
+
+    <p id="app-name"><img src="img/emag2.png" height="40" width="150"/> CAML</p>
+
+    <input type="text" name="search-bar" placeholder="Search:">
+    <i class="fa fa-search"></i>
+</div>
+
+<div class="vertical-menu">
+
+    <a href="#"">Courses</a>
+    <a href="#">Meetings</a>
+    <a href="#">Ideas</a>
+
+</div>
+
+
+</body>
+</html>
